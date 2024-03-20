d5a8e3e0128f60597e3c0dc59abbd64bb01fe701
alessia266 <161491682+alessia266@users.noreply.github.com>
2024-03-20 12:02:30 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+//#include <sstream>
+#include <fstream>
+#include <iomanip>
+//#include <math.h>
+
+double map (double D)
+{
+    return (D-1)*3.0/4.0 - 1.0;
+}
+
+
+int main()
+{
+    std::string filename = "data.csv";
+    std::ifstream ifstr(filename);
+
+    if(ifstr.fail())
+    {
+        std::cerr << "File not found" << std::endl;
+        return 2;
+    }
+
+    std::ofstream ofstr("result.csv");
+    ofstr << "# N Mean" << std::endl;
+
+    double N;
+    double sumValues;
+    int i = 0;
+    while (ifstr >> N)
+    {
+        i++;
+        double mean = 0;
+        sumValues += map(N);
+        mean = sumValues/i;
+        ofstr << i << " " << std::setprecision(16) << std::scientific << mean << std::endl;
+
+    }
+    ifstr.close();
+    ofstr.close();
+
+    return 0;
+}
